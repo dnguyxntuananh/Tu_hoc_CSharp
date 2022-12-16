517719699c42b45b3f83ceca4f0e70576c7d8239
dnguyxntuananh <dta101203@gmail.com>
2022-12-16 21:59:16 +0700

    10:00 pm done

--- Tu_hoc_CShp/Const_ngay16_thang12_2022/Program.cs
+++ Tu_hoc_CShp/Const_ngay16_thang12_2022/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// hằng số
+//const int doSoi = 100;
+//const int doDong = 0;
+//const double pi = 3.14;
+
+//Console.WriteLine("Do soi cua da la: {0}",doSoi);
+//Console.WriteLine("Do dong cua da la: {0}", doDong);
+//Console.WriteLine("gia tri cua pi la: {0}", pi);
+//Console.ReadKey();
+// khi dùng hằng số ( const ) thì không đổi trong suốt quá trình, nên không được đổi giá trị hằng qua các biến gán
+
+//-------------------------------------------------------------
+// các phép toán
+// cho người dùng nhập vào 2 giá trị x, y
+
+int x, y;
+
+Console.WriteLine("nhap x: ");
+x = int.Parse(Console.ReadLine());
+Console.WriteLine("nhap y: ");
+y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("moi nhap dau cua phep tinh");
+string dau = Console.ReadLine();
+switch(dau)
+{
+    case "+": Console.WriteLine(x + y); ; break;
+    case "-": Console.WriteLine(x - y); ; break;
+    case "*": Console.WriteLine(x * y); ; break;
+    case "/": Console.WriteLine(x / y); ; break;
+    case "%": Console.WriteLine(x % y); ; break;
+    default : Console.WriteLine("vui long nhap lai"); ; break;
+}    
